e4ad0af7042e3527074039911bcf29d208c3ece2
Christopher Cates <ccates1@ggc.edu>
2016-09-07 19:23:48 -0400

    Dropping the starter template bomb into the repository

---
+++ app/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
