95df06db6b92b1ba4a7a7ab5bc87eb9449410564
Christopher Cates <ccates1@ggc.edu>
2016-09-24 11:45:34 -0400

    Added the first jquery function for building b and the implementation of the uibModal instance. Need to do this for each of the building objects. Also made an adjustment to the buildings.json format.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ui.bootstrap'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -9,13 +9,36 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
-	$http.get('/buildings.json').then(function(response) {
-		$scope.buildings = response.data;
-	});
+.controller('View1Ctrl', ['$scope', '$http', '$uibModal', function($scope, $http, $uibModal) {
 	$('map').imageMapResize();
-	$('#buildingb').click(function() {
-		console.log('test');
+	var $ctrl = this;
+	$http.get('/buildings.json').then(function(response) {
+		$scope.buildings = response.data.buildings;
+
+		$('#buildingb').click(function() {
+			console.log('test');
+			$scope.showModal();
+		});
 	});
-	
+
+	$scope.showModal = function() {
+		$uibModal.open({
+			animation: true,
+			templateUrl: 'modal.html',
+			controller: 'ModalInstanceCtrl',
+			scope: $scope
+		})
+	};
+
+
+
+}])
+
+.controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
+	$scope.cancel = function() {
+		$uibModalInstance.dismiss('cancel');
+	}
+	$scope.buildingB = $scope.buildings[1];
+	console.log($scope.buildingB);
 }]);
+
