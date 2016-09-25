34cbb7db3150bcb99ebfced0825097d54886292b
aprintz3 <aprintz@ggc.edu>
2016-09-25 12:28:51 -0400

    put the test scripts in a separate folder and fixed the karma conf
    still working on more test

--- karma.conf.js
+++ karma.conf.js
@@ -2,14 +2,16 @@
 module.exports = function(config) {
   config.set({
 
-    basePath: './app',
+    basePath: '',
 
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'components/**/*.js',
-      'view*/**/*.js'
+      'app/components/**/*.js',
+      'app/view1/**/*.js',
+      'app/view2/**/*.js',
+      'testScripts/*.js'
     ],
 
     autoWatch: true,
