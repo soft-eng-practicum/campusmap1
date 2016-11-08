8a828683a0b76060b4245d1c5ecc1b1a94ac2ea7
EricDkim <dkim3@ggc.edu>
2016-11-08 16:10:42 -0500

    removed old functions

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -187,25 +187,6 @@ angular.module('myApp.view1', ['ngRoute', 'ui.bootstrap'])
 
 }]);
 
-//added by eric
-  //added by eric for sidebar functionality
-  //creates the side bar
-  $(document).ready(function(){
-    $('#right-menu').sidr({
-      name: 'sidr-right',
-      side: 'right'
-    });
-  });
-
-  //opens the side bar. works!
-  function openSide(){
-    $.sidr('open', 'sidr-right');
-  }
-
-  // close the side bar. Works
-  function closeSide(){
-    $.sidr('close', 'sidr-right');
-  }
 
 
 
